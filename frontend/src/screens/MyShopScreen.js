e9a60b0595942ec03c8d5115808552e7e68fc63e
victor <a176011@siswa.ukm.edu.my>
2021-11-09 16:44:31 +0800

    MyShop add loading screen

--- frontend/src/screens/MyShopScreen.js
+++ frontend/src/screens/MyShopScreen.js
@@ -11,39 +11,53 @@ const userData = JSON.parse(localStorage.getItem("userData"));
 
 const MyShopScreen = () => {
 
-  const userId = useState (userData.userId);
-
-  const [shop, setShop] = useState([]);
-  let url = `http://localhost:5000/shops/${userId}`;
+  const [shop, setShop] = useState();
+  const [loading, setLoading] = useState(true);
 
   const fetchData = async () =>{
     try{
-      const response = await fetch(url);
+      const response = await fetch(`http://localhost:5000/shops/${userData.userId}`);
       const result = await response.json();
 
+      console.log(result);
       setShop(result);
-      //setLoading(false);
-
-      //checkInStock();
+      setLoading(false);
 
     } catch(error){
       console.log(error);
     }
   }
 
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+
+  // If data not loaded yet, display empty page/loading sign
+  if (loading)
+  {
+    return <div className="loadingscreen">
+    <div className="loading"></div>
+  </div>
+  } 
+
   return (
 
-        <div className="myshopscreen">
+    <div className="myshopscreen">
 
-        <h1>{shop.shopName}</h1>
+      <h1>{shop.shopName}</h1>
+      <p>{shop.shopDescription}</p>
 
-        <Link to={`/myshop/createmyshop`} className="Link" type="button" >
-          <h1>Create MyShop!</h1>
-        </Link>
+  
+      <Link to={`/myshop/createmyshop`} className="Link" type="button" >
+        <h1>Create MyShop!</h1>
+      </Link>
 
-        </div>
+    </div>
 
   )
+  
+  
 
 }
 
