8f83bc3f03709cbda38f20dab48c1d3c912065f8
victor <a176011@siswa.ukm.edu.my>
2022-01-16 09:50:59 +0800

    BuyerOrder fix

--- frontend/src/components/BuyersOrders.js
+++ frontend/src/components/BuyersOrders.js
@@ -1,21 +1,46 @@
 import './BuyerOrders.css';
+import { useEffect, useState } from 'react';
 
-const Orders = ({ _id, userId, productId, quantity, isPaid}) => {
-    var orderPaid;
-        if (isPaid) {
-            orderPaid = "Order paid";
-        } else {
-            orderPaid = "Order not paid";
-        }
+const Orders = ({ _id, userId, productId, quantity, isPaid }) => {
+
+  const [user, setUser] = useState(null);
+
+  var orderPaid;
+  if (isPaid) {
+    orderPaid = "Order paid";
+  } else {
+    orderPaid = "Order not paid";
+  }
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/users/account/${userId}`);
+        const result = await response.json();
+        setUser(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUserData();
+  }, [userId])
 
 
   return (
     <div className="shoporders">
       <div className="orders-info">
         <p className="info-name">Order ID = {_id}</p>
-        <p className="info-name">User ID = {userId}</p>
+
+        {user ? // Some userId's account have been deleted, so thats sometimes why we will get null
+          <div>
+            <p className="info-name">User ID = {userId}</p>
+            <p className='info-name'>{user.name}</p>
+          </div>
+          :
+          <div></div>
+        }
+        
         <p className="info-description"></p>
- 
         <p className="info-price">{orderPaid}</p>
       </div>
     </div>
