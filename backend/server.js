0b6e193ea9fb5df03b0e27a8071247ca5e017682
victorkang516 <66403541+victorkang516@users.noreply.github.com>
2022-05-22 13:51:57 +0800

    new

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://uwushoppingsite.netlify.app/",
+    origin: "https://uwushoppingsite.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
